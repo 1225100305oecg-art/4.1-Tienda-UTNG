c34fbdf66260f8d41e0872c32eb5067b37a463be
1225100305oecg-art <1225100305.oecg@gmail.com>
2026-08-13 17:20:03 -0600

    Create ProductoNoEncontradoException.java

---
+++ src/main/java/oecg/gtid232/ProductoNoEncontradoException.java
@@ -0,0 +1,16 @@
+package oecg.gtid232;
+
+public class ProductoNoEncontradoException extends RuntimeException {
+
+    private final String codigo;
+
+  
+    public ProductoNoEncontradoException(String codigo) {
+        super("Producto no encontrado con codigo: " + codigo);
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+}
